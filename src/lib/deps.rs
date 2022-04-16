d075acd49c118ecf4e562613bef517e43bd3b790
Jérome Eertmans <jeertmans@icloud.com>
2022-04-16 13:42:01 +0200

    feat(lib/deps): nicer formatting for deps

--- src/lib/deps.rs
+++ src/lib/deps.rs
@@ -1,8 +1,36 @@
 use crate::chars::CharStream;
 use crate::token::{TokenKind, TokenStream};
+use lazy_static::lazy_static;
 use regex::Regex;
 use std::fs::read_to_string;
 use std::path::{Path, PathBuf};
+
+lazy_static! {
+    static ref RE_INPUT: Regex = Regex::new(r"\\input\{(.*)\}").unwrap();
+    static ref RE_IMAGE: Regex = Regex::new(r"\\includegraphics(?:\[.*\])\{([^\}]*)\}").unwrap();
+}
+
+
+trait PathUtils {
+    fn with_default_extension(self, ext: &str) -> PathBuf;
+    fn with_main_dir(&self, main_dir: &PathBuf) -> PathBuf;
+}
+
+impl PathUtils for PathBuf {
+    fn with_default_extension(mut self, ext: &str) -> PathBuf {
+        if self.extension().is_none() {
+            self.set_extension(ext);
+        }
+        self
+    }
+    fn with_main_dir(&self, main_dir: &PathBuf) -> PathBuf {
+        if self.is_relative() {
+            main_dir.join(self)
+        } else {
+            self.clone()
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 enum DependencyKind {
@@ -12,62 +40,52 @@ enum DependencyKind {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-struct Dependency {
-    filename: String,
-    main: String,
+struct Dependency<'source> {
+    filename: PathBuf,
+    main_dir: &'source Path,
     dependencies: Vec<Self>,
     kind: DependencyKind,
 }
 
-fn relative_path(parent: &str, child: &str, ext: &str) -> String {
-    let mut child = PathBuf::from(child);
-
-    if child.extension().is_none() {
-        child.set_extension(ext);
-    }
-
-    let child = Path::new(parent).parent().unwrap().join(child);
-
-    child.to_str().unwrap().to_string()
-}
-
-impl Dependency {
-    pub fn new(filename: &str, main: &str) -> Self {
+impl<'source> Dependency<'source> {
+    pub fn new(filename: PathBuf, main_dir: &'source PathBuf) -> Self {
         let mut dependencies = Vec::<Dependency>::new();
 
-        if !filename.ends_with(".tex") {
-            return Self {
-                filename: filename.to_string(),
-                main: main.to_string(),
-                dependencies,
-                kind: DependencyKind::Other,
-            };
-        }
+        let kind = match filename
+            .extension()
+            .expect(&format!("filename `{:?}` has no extension", filename))
+            .to_str()
+            .unwrap()
+        {
+            "tex" => {
+                let filepath = filename.with_main_dir(main_dir);
+                let contents =
+                    read_to_string(&filepath).expect(&format!("Could not read {:?}", filepath));
 
-        let contents = read_to_string(filename).expect(&format!("Could not read {}", filename));
+                let token_stream: TokenStream = CharStream::new(&contents).into();
 
-        let token_stream: TokenStream = CharStream::new(&contents).into();
-
-        let re_input = Regex::new(r"\\input\{(.*)\}").unwrap();
-        let re_image = Regex::new(r"\\includegraphics(?:\[.*\])\{([^\}]*)\}").unwrap();
-
-        for token in token_stream {
-            if token.kind == TokenKind::Command {
-                if let Some(caps) = re_input.captures(token.slice) {
-                    let dep_filename = relative_path(main, caps.get(1).unwrap().as_str(), "tex");
-                    dependencies.push(Dependency::new(&dep_filename, main));
-                } else if let Some(caps) = re_image.captures(token.slice) {
-                    let dep_filename = relative_path(main, caps.get(1).unwrap().as_str(), "pdf");
-                    dependencies.push(Dependency::new(&dep_filename, main));
+                for token in token_stream {
+                    if token.kind == TokenKind::Command {
+                        if let Some(caps) = RE_INPUT.captures(token.slice) {
+                            let dep_filename = PathBuf::from(&caps[1]).with_default_extension("tex");
+                            dependencies.push(Dependency::new(dep_filename, main_dir));
+                        } else if let Some(caps) = RE_IMAGE.captures(token.slice) {
+                            let dep_filename = PathBuf::from(&caps[1]).with_default_extension("pdf");
+                            dependencies.push(Dependency::new(dep_filename, main_dir));
+                        }
+                    }
                 }
+                DependencyKind::TeX
             }
-        }
+            "jpeg" | "jpg" | "png" | "pdf" | "svg" => DependencyKind::Image,
+            _ => DependencyKind::Other,
+        };
 
         Self {
-            filename: filename.to_string(),
-            main: main.to_string(),
+            filename,
+            main_dir,
             dependencies,
-            kind: DependencyKind::TeX,
+            kind,
         }
     }
 }
@@ -81,7 +99,10 @@ fn write_deps(dep: &Dependency, depth: usize) {
 }
 
 pub fn file_deps(filename: &str) {
-    let main_dep = Dependency::new(filename, filename);
+    println!("Depedencies of {}:", filename);
+    let filename = PathBuf::from(filename);
+    let main_dir = filename.parent().unwrap().into();
+    let main_dep = Dependency::new(filename.clone(), &main_dir);
 
     write_deps(&main_dep, 0);
 }
